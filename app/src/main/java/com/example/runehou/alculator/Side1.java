27d67b6e66b1170a55cb7a3adac44bd61b6bbd99
Rune Hou Thode <runehouthode@gmail.com>
2017-07-22 09:37:45 +0800

    Initial commit

---
+++ app/src/main/java/com/example/runehou/alculator/Side1.java
@@ -0,0 +1,5 @@
+package com.example.runehou.alculator;
+
+public class Side1 {
+
+}
---
+++ app/src/main/java/com/example/runehou/alculator/side2.java
@@ -0,0 +1,8 @@
+package com.example.runehou.alculator;
+
+import android.widget.TextView;
+
+public class side2 {
+
+    private TextView view;
+}
